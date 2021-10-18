4e121514c790246fc24d18eeaea1daf351ee4336
Shy_Same <olyalychko@gmail.com>
2021-10-18 16:09:12 +0300

    serializer post+comments

--- post/serializers.py
+++ post/serializers.py
@@ -13,24 +13,25 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'username', 'last_name', 'email']
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True)
-    date_create = serializers.DateField(read_only=True)
-    author = serializers.CharField(read_only=True)
-    text = serializers.CharField(required=True)
-
-    class Meta:
-        model = Post
-        fields = ['id', 'url', 'title', 'date_create', 'author', 'text']
-
-
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
+    pk = serializers.IntegerField(read_only=True)
     message = serializers.CharField(required=True)
     date_create = serializers.DateTimeField(read_only=True)
     author = serializers.CharField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['id', 'url', 'message', 'date_create', 'author', 'post', ]
+        fields = ['pk', 'url', 'message', 'date_create', 'author', 'post', ]
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True)
+    date_create = serializers.DateField(read_only=True)
+    author = serializers.CharField(read_only=True)
+    text = serializers.CharField(required=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['pk', 'url', 'title', 'date_create', 'author', 'text', 'comments']
